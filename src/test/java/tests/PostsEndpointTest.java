11fba790dd3294bc9295d9fba8b50e3be9864b6d
Seinia <vlad.yacevilov@gmail.com>
2025-03-31 10:45:16 +0300

    Implemented explicit waits for demonstrating of concurrent execution

--- src/test/java/tests/PostsEndpointTest.java
+++ src/test/java/tests/PostsEndpointTest.java
@@ -2,8 +2,11 @@ package tests;
 
 import model.Post;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import service.PostService;
 
+import java.time.LocalTime;
 
 public class PostsEndpointTest {
 
@@ -11,6 +14,12 @@ public class PostsEndpointTest {
 
     @Test
     public void getAllPostsAndVerifyStatus(){
+        logTest("Test 1 for Posts");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         postService.getAllPosts()
                 .then().log().body()
                 .assertThat()
@@ -19,6 +28,12 @@ public class PostsEndpointTest {
 
     @Test
     public void getSpecifiedPostAndVerifyStatus(){
+        logTest("Test 2 for Posts");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         postService.getPostById(1)
                 .then().log().body()
                 .assertThat()
@@ -27,6 +42,12 @@ public class PostsEndpointTest {
 
     @Test
     public void createSpecifiedPostAndVerifyStatus(){
+        logTest("Test 3 for Posts");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         postService.createPost(
                 Post.builder()
                         .title("My first post API test")
@@ -39,6 +60,12 @@ public class PostsEndpointTest {
 
     @Test
     public void updateSpecifiedPostAndVerifyStatus(){
+        logTest("Test 4 for Posts");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         postService.updatePost(
                 Post.builder()
                         .body("RestAssured helped me to do this task")
@@ -51,9 +78,19 @@ public class PostsEndpointTest {
 
     @Test
     public void deleteSpecifiedPostAndVerifyStatus(){
+        logTest("Test 5 for Posts");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         postService.deletePost(1)
                 .then().assertThat()
                 .statusCode(200);
+    }
+
+    private void logTest(String testName) {
+        System.out.println(testName + " - Start: " + LocalTime.now() + " - Thread: " + Thread.currentThread().getId());
     }
 
 }
--- src/test/java/tests/UsersEndpointTest.java
+++ src/test/java/tests/UsersEndpointTest.java
@@ -1,10 +1,13 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import service.UserService;
 
-import static org.hamcrest.Matchers.hasSize;
+import java.time.LocalTime;
 
+import static org.hamcrest.Matchers.hasSize;
 
 public class UsersEndpointTest {
 
@@ -12,24 +15,47 @@ public class UsersEndpointTest {
 
     @Test
     public void verifyStatusCodeResponse(){
+        logTest("Test 1 for Users");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         userService.getAllUsers()
-                .then().assertThat()
+                .then()
+                .assertThat()
                 .statusCode(200);
     }
 
     @Test
     public void verifyHeaderResponse(){
+        logTest("Test 2 for Users");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         userService.getAllUsers()
-                .then().assertThat()
-                .contentType(
-                        "application/json; charset=utf-8"
-                );
+                .then()
+                .assertThat()
+                .contentType("application/json; charset=utf-8");
     }
 
     @Test
     public void verifyArrayResponseSize(){
+        logTest("Test 3 for Users");
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         userService.getAllUsers()
-                .then().assertThat()
+                .then()
+                .assertThat()
                 .body("", hasSize(10));
+    }
+
+    private void logTest(String testName) {
+        System.out.println(testName + " - Start: " + LocalTime.now() + " - Thread: " + Thread.currentThread().getId());
     }
 }
